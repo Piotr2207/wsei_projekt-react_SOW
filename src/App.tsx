c58f291c8727089975be22ef4d8d1b66102f0db3
Piotr2207 <67909472+Piotr2207@users.noreply.github.com>
2021-06-26 15:15:15 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { fetchUsers } from "./redux/Actions/userActions";
-import { LeftMenu } from "./Component/LeftMenu/LeftMenu";
-import { TopNav } from "./Component/TopNav/TopNav";
-import HomePage from "./Views/HomePage";
-import Publications from "./Views/Publications";
-import Ecosystem from "./Views/Ecosystem";
-import Entities from "./Views/Entities";
-import Test from "./Views/Test";
-import Profile from "./Views/Profile";
-import Workspaces from "./Views/Workspaces";
+import React from 'react';
+import './App.css';
+import MainPage from './components/MainPage/MainPage';
+
 function App() {
   return (
-    <Router>
-      <TopNav />
-      <LeftMenu />
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/publications">
-          <Publications />
-        </Route>
-        <Route path="/ecosystem">
-          <Ecosystem />
-        </Route>
-        <Route path="/entities">
-          <Entities />
-        </Route>
-        <Route path="/test">
-          <Test />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/workspaces">
-          <Workspaces />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <MainPage/>
+    </div>
   );
 }
 
-export default App;
