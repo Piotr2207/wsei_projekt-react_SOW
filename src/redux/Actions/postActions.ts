693442ac62c05a62b23e67b3e8748c6acc404b0e
Piotr2207 <67909472+Piotr2207@users.noreply.github.com>
2021-06-26 15:26:25 +0200

    Update postActions.ts

--- src/redux/Actions/postActions.ts
+++ src/redux/Actions/postActions.ts
@@ -1,15 +1,15 @@
-import { Dispatch } from "redux";
-import * as actionTypes from "../Types/postTypes";
-import { ILatestPublicationPost } from "../../StyleHelpers/ApiInterfaces";
+import { Dispatch } from 'redux';
+import * as actionsTypes from '../actions/actionTypes/postTypes';
+import { ISinglePost } from '../entities/ISinglePost';
 
-export const fetchPosts = (): Promise<ILatestPublicationPost[]> =>
-  ((dispatch: Dispatch) => {
-    return fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then((response) => response.json())
-      .then((post: ILatestPublicationPost[]) => {
-        dispatch({
-          type: actionTypes.FETCH_POST,
-          post,
-        });
-      });
-  }) as any;
+export const getPosts = () : Promise<ISinglePost[]> => ((dispatch:Dispatch) =>{
+
+        return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then((postList : ISinglePost[]) => {
+            dispatch({
+                type:actionsTypes.GET_POST,
+                postList
+            })
+        })
+}) as any;
