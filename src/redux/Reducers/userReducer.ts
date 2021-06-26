1c09042f5f8e1613b6bb7de0c623d14157835dbd
Piotr2207 <67909472+Piotr2207@users.noreply.github.com>
2021-06-26 15:28:14 +0200

    Update userReducer.ts

--- src/redux/Reducers/userReducer.ts
+++ src/redux/Reducers/userReducer.ts
@@ -1,50 +1,26 @@
-import { IUser } from "../../StyleHelpers/ApiInterfaces";
-import * as actionTypes from "../Types/userTypes";
+import { ISingleUser } from '../entities/users';
+import * as actionTypes from '../actions/actionTypes/userTypes';
 
-export interface IUserReducer {
-  users: IUser[];
-  user: IUser;
+export interface IUsersReducer {
+    usersList: ISingleUser[];
 }
 
-const defaultState = (): IUserReducer => ({
-  users: [],
-  user: {
-    id: 1,
-    name: "",
-    username: "",
-    email: "",
-    address: {
-      street: "",
-      suite: "",
-      city: "",
-      zipcode: "",
-    },
-    phone: "",
-    website: "",
-    company: { name: "", catchPhrase: "", bs: "" },
-  },
+const defaultState = (): IUsersReducer => ({
+    usersList: [],
 });
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState(), action: any) => {
-  switch (action.type) {
-    case actionTypes.FETCH_USERS: {
-      const data: actionTypes.IUserTypes["FETCH_USERS"] = action;
-      return {
-        ...state,
-        users: data.users,
-      };
+    switch(action.type) { 
+        case actionTypes.GET_USERS:{
+            const data: actionTypes.IUserTypes['GET_USERS'] = action;
+            return {
+                ...state,
+                usersList: data.usersList
+            }       
+        }
+
+        default:
+            return state;
+        }
     }
-    case actionTypes.FETCH_USER: {
-      const data: actionTypes.IUserTypes["FETCH_USER"] = action;
-      return {
-        ...state,
-        user: data.user,
-      };
-    }
-    default: {
-      return {
-        ...state,
-      };
-    }
-  }
-};
