950a6e5a97c1b0a3d4bb1c411718594c5fcf008c
Piotr2207 <67909472+Piotr2207@users.noreply.github.com>
2021-06-26 15:25:58 +0200

    Update userActions.ts

--- src/redux/Actions/userActions.ts
+++ src/redux/Actions/userActions.ts
@@ -1,27 +1,16 @@
-import { Dispatch } from "redux";
-import * as actionTypes from "../Types/userTypes";
-import { IUser } from "../../StyleHelpers/ApiInterfaces";
+import { Dispatch } from 'redux';
+import * as actionTypes from '../actions/actionTypes/userTypes';
+import { ISingleUser } from '../entities/users';
 
-export const fetchUsers = (): Promise<IUser[]> =>
-  ((dispatch: Dispatch) => {
-    return fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((users: IUser[]) => {
-        dispatch({
-          type: actionTypes.FETCH_USERS,
-          users,
-        });
-      });
-  }) as any;
-export const fetchUser = (id: string): Promise<IUser> =>
-  ((dispatch: Dispatch) => {
-    return fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((response) => response.json())
-      .then((user: IUser) => {
-        dispatch({
-          type: actionTypes.FETCH_USER,
-          user,
-        });
-      });
-  }) as any;
-//Math.floor(Math.random() * data.length)
+export const getUsers = (): Promise<ISingleUser[]> => ((dispatch: Dispatch) => {
+
+    return fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((usersList: ISingleUser[]) => {
+            dispatch({
+                type: actionTypes.GET_USERS,
+                usersList
+            })
+        })
+        .catch(err => console.log(err))
+    }) as any;
